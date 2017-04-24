69fd0b70ab60a3a3e8ae1ad4bd03f4e319967805
Patrick Jackson <pj@solidhex.com>
2017-04-24 12:45:14 -0700

    mobile and tablet view work parent

--- work-parent.php
+++ work-parent.php
@@ -10,11 +10,13 @@
 <div class="work-parent">
 	<div class="work-parent-content">
 		<div class="work-feature">
-			<figure>
-				<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
-			</figure>
-			<div class="work-feature-copy">
-				<h2>Strategic Branding that Resonates<a href="" class="read-more">Read <br>More</a></h2>
+			<div class="work-image-wrap">
+				<figure class="work-feature-image">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+				</figure>
+				<div class="work-heading">
+					<h2>Strategic Branding that Resonates<a href="" class="read-more">Read <br>More</a></h2>
+				</div>
 			</div>
 			<div class="more">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut viverra consectetur turpis hendrerit dictum. Ut euismod urna in massa dapibus, vel finibus lorem tristique. Sed venenatis ligula ut nunc feugiat laoreet. Donec condimentum tempor neque, vitae luctus sem vestibulum semper. Ut varius, ligula vel feugiat faucibus, quam felis congue tellus, in placerat erat nisi laoreet orci. Fusce auctor enim nec sollicitudin cursus. Praesent suscipit scelerisque facilisis. Vestibulum porttitor elit vitae nulla semper, ut hendrerit urna posuere. Edit this content in the admin under Pages > Hom</div>
 		</div>
@@ -24,6 +26,15 @@
 	<h2>All Projects</h2>
 	<a href="#" class="view-by-category">view projects by category</a>
 	<ul class="project-list">
+		<li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li>
+		<li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li>
+		<li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li>
 		<li>
 			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
 		</li>
