005a50bcf6a8e6dfbf308572f5c3cd162dc94452
Patrick Jackson <pj@solidhex.com>
2017-04-25 12:23:57 -0700

    work detail mobile - less shown view

--- work-detail.php
+++ work-detail.php
@@ -6,7 +6,43 @@
 
 <?php get_header(); ?>
 
-content
 
+<div class="work-detail">
+	<div class="work-detail-hero">
+		<figure>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</figure>
+	</div>
+	<div class="slider">
+		<h2>New York Stock Exchange</h2>
+		<div class="intro">Authenticity, vibrancy and joie de vivre drive the brand strategy and 
+graphics for the surf brand Roxy.</div>
+		<a href="" class="read-more alt">Read More</a>
+		<div class="supplemental">
+			
+		</div>
+	</div>
+	
+</div>
+
+<div class="projects-sort-container">
+	<h2>All Projects</h2>
+	<a href="#" class="view-by-category">view projects by category</a>
+	<ul class="project-list">
+		<li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li>
+	</ul>
+</div>
 
 <?php get_footer(); ?>
