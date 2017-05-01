916d0d8235ff0c3a26a08925df525aa2643ae11e
Patrick Jackson <pj@solidhex.com>
2017-05-01 09:33:37 -0700

    add projects sort results

--- projects.php
+++ projects.php
@@ -5,5 +5,23 @@
 ?>
 
 <?php get_header(); ?>
-	Projects page.
+<div class="projects-sort-container">
+	<h2>Projects</h2>
+	<a href="#" class="view-by-category">view projects by category</a>
+	<ul class="project-list">
+		<li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li><li>
+			<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
+		</li>
+	</ul>
+</div>
 <?php get_footer(); ?>
