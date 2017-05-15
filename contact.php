7e351b26b93af257eb1503f94a8e370de776fd98
Patrick Jackson <pj@solidhex.com>
2017-05-15 10:48:33 -0700

    contact + work updates.

--- contact.php
+++ contact.php
@@ -7,18 +7,24 @@
 <?php get_header(); ?>
 
 <div class="contact-content">
-	<h2>General Inquiries</h2>
-	<a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#107;&#101;&#110;&#100;&#114;&#097;&#105;&#110;&#109;&#097;&#110;&#046;&#099;&#111;&#109;">hello@kendrainman.com</a>
+	<?php if ( is_page('contact') ): ?>
+		<h2>General Inquiries</h2>
+		<a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#107;&#101;&#110;&#100;&#114;&#097;&#105;&#110;&#109;&#097;&#110;&#046;&#099;&#111;&#109;">hello@kendrainman.com</a>
 
-	<div class="connect">
-		<h2>Let's Connect</h2>
+		<div class="connect">
+			<h2>Let's Connect</h2>
 
-		<nav>
-			<a href="https://www.instagram.com/kendrainman/" rel="external">Instagram</a>
-			<a href="https://www.pinterest.com/kendrai/" rel="external">Pinterest</a>
-			<a href="https://www.linkedin.com/in/kendrainman/" rel="external">Linked In</a>
-		</nav>
-	</div>
+			<nav>
+				<a href="https://www.instagram.com/kendrainman/" rel="external">Instagram</a>
+				<a href="https://www.pinterest.com/kendrai/" rel="external">Pinterest</a>
+				<a href="https://www.linkedin.com/in/kendrainman/" rel="external">Linked In</a>
+			</nav>
+		</div>
+	<?php else: ?>
+		<h2>Request to View my Portfolio</h2>
+		<a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#107;&#101;&#110;&#100;&#114;&#097;&#105;&#110;&#109;&#097;&#110;&#046;&#099;&#111;&#109;">hello@kendrainman.com</a>
+	<?php endif ?>
+	
 </div>
 
 <?php get_footer(); ?>
--- work-parent.php
+++ work-parent.php
@@ -6,42 +6,7 @@
 	get_header();
 
 ?>
-<div class="work-parent">
-	<div class="work-parent-content">
-		<div class="work-feature">
-			<div class="work-image-wrap">
-				<figure class="work-feature-image">
-					<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/fpo-work-large.jpg">
-				</figure>
-				<div class="work-heading">
-					<h2>Strategic Branding that Resonates<a href="" class="read-more"><span>Read</span> <span>More</span></a></h2>
-				</div>
-			</div>
-			<?php while ( have_posts() ) : the_post(); ?>
-				
-				<div class="more"><?php the_content(); ?></div>
-			
-			<?php endwhile; ?>
-		</div>
-	</div>
-</div>
-<div class="projects-sort-container">
-	<h2>All Projects</h2>
-	<a href="#" class="view-by-category">view projects by category</a>
-	<ul class="project-list">
-		<?php 
-			$all_projects = get_pages( array( 'child_of' => 22) );
-			foreach ($all_projects as $project) {
-				if ($project->post_parent !== 22) {
-		?>
-			<li><a href="<?php echo get_page_link( $project->ID ); ?>"><?php echo get_the_post_thumbnail($project->ID, 'small'); ?></a></li>
-		<?php 
-				}
-			} 
-		?>
-		
-	</ul>
-</div>
+
 
 
 
