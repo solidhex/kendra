db4a187dede8905db62f6f0b546e95af8cb72ec8
Patrick Jackson <pj@solidhex.com>
2017-06-28 20:20:43 -0700

    adjust video.

--- assets/js/site.js
+++ assets/js/site.js
@@ -30,12 +30,12 @@
 		
 		var $container = $('.slider-wrap'),
 			$message = $('.home-hero-message');
-		// test comment
 		$('.home-hero-media > .slides').bxSlider({
 			pager: false,
 			auto: false,
 			mode: "fade",
-			video: true, 
+			video: true,
+			useCSS: false,
 			speed: 800,
 			nextSelector: '#slider-next',
 			prevSelector: '#slider-prev',
